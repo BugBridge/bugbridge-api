d0fef4f6cd0b725e4810e7b56074b375d4de0af4
SowinskiBraeden <braeden@sowinski.dev>
2025-10-04 17:41:03 -0700

    tidy template.go

--- models/template.go
+++ models/template.go
@@ -1,13 +1,9 @@
 package models
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Template struct {
-	Title				string	`json:"title" bson:"Title"`
-	Desc				string	`json:"desc" bson:"Desc"`
-	Steps				string	`json:"steps" bson:"Steps"`
-	Behaviour			string	`json:"behaviour" bson:"Behaviour"`
-	AdditionalInfo		string	`json:"addInfo" bson:"AdditionalInfo"`
+	Title          string `json:"title"     bson:"title"`
+	Desc           string `json:"desc"      bson:"desc"`
+	Steps          string `json:"steps"     bson:"steps"`
+	Behaviour      string `json:"behaviour" bson:"behaviour"`
+	AdditionalInfo string `json:"addInfo"   bson:"additionalInfo"`
 }
