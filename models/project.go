0413069f72d742f405c3412cfb2014dfba3d6afd
JoaquinPar <jparedes.peake@gmail.com>
2025-10-04 20:18:36 -0700

    update Project.go file

--- models/project.go
+++ models/project.go
@@ -21,3 +21,11 @@ type ProjectDetails struct {
 	Template string `json:"template"`
 	OwnerID  string `json:"adminID"`
 }
+
+type Template struct {
+	Title          string `json:"title"     bson:"title"`
+	Desc           string `json:"desc"      bson:"desc"`
+	Steps          string `json:"steps"     bson:"steps"`
+	Behaviour      string `json:"behaviour" bson:"behaviour"`
+	AdditionalInfo string `json:"addInfo"   bson:"additionalInfo"`
+}
