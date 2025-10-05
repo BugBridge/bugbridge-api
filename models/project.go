16f370fd0c118438823f50c92e9f3a68b2eefbb8
JoaquinPar <jparedes.peake@gmail.com>
2025-10-04 20:16:31 -0700

    moved Template under Project

--- models/project.go
+++ models/project.go
@@ -3,14 +3,15 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Project struct {
-	ID         primitive.ObjectID `json:"_id"       bson:"_id"`      //Id of a project
-	Name       string             `json:"name"      bson:"name"`     //Name of project
-	Desc       string             `json:"desc"      bson:"desc"`     // Public description of the project
-	Template   string             `json:"template"  bson:"template"` // Template that bug reports should be submitted
-	OwnerID    string             `json:"ownerId"   bson:"ownerId"`
-	AdminsIDs  []string           `json:"adminIds"  bson:"adminIds"`  //Array of IDs for users as admins
-	MembersIDs []string           `json:"memberIds" bson:"memberIds"` //Array of IDs of users
-	ReportIDs  []string           `json:"reportIds" bson:"reportIds"` //Array of IDs of reports
+	ID         	primitive.ObjectID 	`json:"_id"       bson:"_id"`      //Id of a project
+	Name       	string             	`json:"name"      bson:"name"`     //Name of project
+	Desc       	string             	`json:"desc"      bson:"desc"`     // Public description of the project
+	Template   	string             	`json:"template"  bson:"template"` // Template that bug reports should be submitted
+	OwnerID    	string             	`json:"ownerId"   bson:"ownerId"`
+	AdminsIDs  	[]string           	`json:"adminIds"  bson:"adminIds"`  //Array of IDs for users as admins
+	MembersIDs 	[]string           	`json:"memberIds" bson:"memberIds"` //Array of IDs of users
+	ReportIDs  	[]string           	`json:"reportIds" bson:"reportIds"` //Array of IDs of reports
+	Template 	Template 			`json:"template"  bson:"template"`
 }
 
 // Data structure of the json object received in POST to create project
--- models/template.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Template struct {
-	Title          string `json:"title"     bson:"title"`
-	Desc           string `json:"desc"      bson:"desc"`
-	Steps          string `json:"steps"     bson:"steps"`
-	Behaviour      string `json:"behaviour" bson:"behaviour"`
-	AdditionalInfo string `json:"addInfo"   bson:"additionalInfo"`
-}
